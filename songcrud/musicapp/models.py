a4ad6acf723c92b1b9698ae36333d4dd428b46de
Prideland-Okoi <prideland.okoi@gmail.com>
2022-10-24 00:10:23 +0000

    models created

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    title = models.CharField(max_length=50, null=True)
+    date_released = models.CharField(max_length=50, null=True)
+    likes = models.PositiveIntegerField()
+    artist_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.PositiveIntegerField()
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
 # Create your views here.
+
+def index(request):
+   # return render(request, 'musicapp/musicapp.html', context)
+   return render(request, 'musicapp.html')
