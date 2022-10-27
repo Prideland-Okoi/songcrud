30aab540e130ea4ca098d77ac4de82d978549f0c
Prideland-Okoi <prideland.okoi@gmail.com>
2022-10-27 08:01:11 +0000

    link urls of the pages

--- songcrud/musicapp/urls.py
+++ songcrud/musicapp/urls.py
@@ -5,4 +5,10 @@ app_name = 'musicapp'
 
 urlpatterns = [
         path("", views.index, name = 'musicapp'),
+        path("", views.albumsstoreview, name = 'albums-store'),
+        path("", views.blogview, name = 'blog'),
+        path("", views.contactview, name = 'contact'),
+        path("", views.elementsview, name = 'elements'),
+        path("", views.eventview, name = 'event'),
+        path("", views.login, name = 'login'),
 ]
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -4,5 +4,22 @@ from django.http import HttpResponse, request
 # Create your views here.
 
 def index(request):
-   # return render(request, 'musicapp/musicapp.html', context)
    return render(request, template_name='musicapp/index.html')
+
+def blogview(request):
+   return render(request, template_name='musicapp/blog.html')
+
+def contactview(request):
+   return render(request, template_name='musicapp/contact.html')
+
+def elementsview(request):
+   return render(request, template_name='musicapp/elements.html')
+
+def eventview(request):
+   return render(request, template_name='musicapp/events.html')
+
+def login(request):
+   return render(request, template_name='musicapp/login.html')
+
+def albumsstoreview(request):
+   return render(request, template_name='musicapp/albums-store.html')
